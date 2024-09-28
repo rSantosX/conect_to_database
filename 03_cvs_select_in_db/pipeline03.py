c293157415684a0674ecd2857208724af4deab09
Rodrigo <rsvitorrs@gmail.com>
2024-09-28 12:55:12 -0300

    Att CSV Select IN Data Base

--- 03_cvs_select_in_db/pipeline03.py
+++ 03_cvs_select_in_db/pipeline03.py
@@ -3,28 +3,42 @@ import warnings
 import psycopg2 # type: ignore
 import pandas as pd # type: ignore
 from dotenv import load_dotenv # type: ignore
-import sqlite3  # Replace with your database connector (e.g., psycopg2 for PostgreSQL)
 
-# Step 1: Read data from the CSV file
-csv_df = pd.read_csv("customers.csv")  # Replace with your .csv file path
+# Step 1: Carregar variáveis ​​de ambiente do arquivo .env
+load_dotenv()
 
-# Step 2: Extract the list of customer IDs from the CSV file
-customer_ids = csv_df['customer_id'].tolist()  # Convert the column to a list
+# Step 2: Recuperar as variáveis
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
-# Step 3: Ensure there are IDs to use in the query
-if len(customer_ids) == 0:
-    print("No customer IDs found in the CSV file.")
-else:
-    # Assuming `conn` is your database connection object (adjust the placeholder based on your database type)
-    # For this example, I'm using a SQLite in-memory database, but replace this with your actual database connection
-    conn = sqlite3.connect(":memory:")
 
-    # Step 4: Build the query dynamically using `%s` for the `IN` clause
-    placeholders = ', '.join(['%s'] * len(customer_ids))  # For PostgreSQL, MySQL, use %s
-    query = f"SELECT * FROM orders WHERE customer_id IN ({placeholders})"
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
 
-    # Step 5: Use `tuple` to pass the list of IDs as parameters
-    orders_df = pd.read_sql_query(query, conn, params=tuple(customer_ids))  # Pass `customer_ids` as a tuple
+# Step 1: Leio os dados do CSV e tranformo em um DataFrame
+path_csv = '../03_cvs_select_in_db/csv/AmostraCNPJ.csv'
+csv_df = pd.read_csv(path_csv, sep=';', dtype={'recpjcnscnpj': str}) # Uso o dtype para definir o tipo da coluna recpjcnscnpj como str
+
+# Step 2: Extraio a lista de Documentos do arquivo CSV
+customer_docs = csv_df['recpjcnscnpj'].tolist()  # Converto a coluna para uma lista
+
+# Step 4: Build the query dynamically using `%s` for the `IN` clause
+placeholders = ', '.join(['%s'] * len(customer_docs))  # For PostgreSQL, MySQL, use %s
+query = f"SELECT * FROM receita_cnpj.recpjcns WHERE recpjcnscnpj IN ({placeholders})"
+
+
+# Step 5: Use `tuple` to pass the list of IDs as parameters
+orders_df = pd.read_sql_query(query, conn, params=list(customer_docs))  # Pass `customer_ids` as a tuple
 
     # Step 6: Output the resulting DataFrame
-    print(orders_df)
+print(orders_df)
+
+conn.close()
